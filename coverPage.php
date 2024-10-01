67881c1e0470a67d148456e09901994eaabb08b9
Charlie Starling <charliejackstarling1@gmail.com>
2024-10-01 18:52:41 +0100

    dfghjk

--- coverPage.php
+++ coverPage.php
@@ -11,65 +11,15 @@
 //     header("Location: m_home.php");
 //   }
 // }
-$pageTitle = 'Home';
 include 'header.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>HMS :: Register</title>
-  <?php include('includes/header.php'); ?>
-
 </head>
 <body>
   <div class="login-container" id="registerLogin">
-
-    <?php 
-    if (isset($_POST["submit"])){
-      $first_name= $_POST["forename"];
-      $surname= $_POST["surname"];
-      $email_address= $_POST["email"];
-      $password= $_POST["password"];
-      $street_address= $_POST["address"];
-      $postcode= $_POST["postcode"];
-      $role = $_POST["role"];
-      $errors = array();
-      
-      $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-
-      require_once "includes/config.php";
-      $sql_check_email= "SELECT * FROM users WHERE email_address = '$email_address'";
-      $email_result= mysqli_query($conn, $sql_check_email);
-      $email_row_count= mysqli_num_rows($email_result);
-      if ($email_row_count>0){
-        array_push($errors, "Email already exists!");
-      }
-
-      if (count($errors)>0){
-        foreach($errors as $error){
-        echo "<p class='error_message'>ERROR: $error.</p";
-        }
-      }
-      else{
-        $sql = "INSERT INTO registrations (first_name, surname, email_address, password, street_address, post_code, user_role)
-        VALUES (?, ?, ?, ?, ?, ?, ?)";
-        $stmt = mysqli_stmt_init($conn);
-        $prepare_stmt = mysqli_stmt_prepare($stmt, $sql);
-        if($prepare_stmt){
-          mysqli_stmt_bind_param($stmt, "sssssss", $first_name, $surname, $email_address, $passwordHash, $street_address, $postcode, $role);
-          mysqli_stmt_execute($stmt);
-
-          
-
-          echo "<p>Registered Successfully</p>";
-        } else{
-          die("Something went wrong.");
-        }
-      }   
-    }   
-    ?>
-
     <h1>HMS - Register New Account</h1>
     <form action="register.php" method="post">
       <input type="name" id="register_first_name" name="forename" placeholder="First Name" required>
