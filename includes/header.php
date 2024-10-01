b39e16bd3118b2e6db5c226b90626074ea5d72a0
Charlie Starling <charliejackstarling1@gmail.com>
2024-10-01 18:41:40 +0100

    asdfd

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="" type="text/css" rel="stylesheet">
+  <link href="../styles/desktop.css" type="text/css" rel="stylesheet">
 
   <!-- <script defer src="js/script.js"></script> -->
   <title><?php echo isset($pageTitle) ? $pageTitle:"Trade Point";?></title>
