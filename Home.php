9a148681053f6a1dfcd86c39d8a4fff7ecdd11ac
Charlie Starling <charliejackstarling1@gmail.com>
2024-10-04 15:39:39 +0100

    Signout on home.php

--- Home.php
+++ Home.php
@@ -7,3 +7,16 @@ $title = "TradePoint :: Home";
 require('includes/header.php');
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <p><a href="Logout.php">Log out</a></p>
+</head>
+<body>
+    
+</body>
+</html>
+
