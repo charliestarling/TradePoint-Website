786876322c6744243715dc953bf1d1f575220b0e
Charlie Starling <charliejackstarling1@gmail.com>
2024-10-13 00:50:34 +0100

    coverpage

--- coverPage.php
+++ coverPage.php
@@ -8,5 +8,9 @@ require('includes/header.php');
 <head>
 </head>
 <body>
+    <div class="topic-heading">
+        <h1>Trade Point</h1>
+        <h4>All of your jobs in one platform</h4>
+    </div>
 </body>
 </html>
