4f7817fdc288a41be9f837c4ff4b4e3fbe746a7a
Charlie Starling <charliejackstarling1@gmail.com>
2024-10-13 04:11:15 +0100

    sdf

--- pages-system/Home.php
+++ pages-system/Home.php
@@ -22,3 +22,8 @@ require_once('includes/header-system.php');
 </body>
 </html>
 
+<!-- TO DO  -->
+<!-- For some reason, the server can't find some of the pages after logging in, -->
+<!-- I suspect it's something to do with the directories / relative paths to each -->
+<!-- file. I get an error 500 each time. I'm suspecting the includes & require methods too -->
+<!-- Got to go to bed. It's just hit 04:10 on 13/10/24 -->
